731c1b97038b573cf25505f2cd3f4d3668638884
Deepa <kosalai17@gmail.com>
2021-07-13 09:54:56 -0700

    route between html and server

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const path = require('path');
 const express = require('express'); //dependencies
 const PORT = process.env.PORT || 3001;
 const app = express(); // instantiate the server
+const { animals } = require('./data/animals'); // data from animals.json
+
+//Middleware
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-const { animals } = require('./data/animals'); // data from animals.json
+// to include assets like css for html-to access front end code
+app.use(express.static('public'));
+
 
 //get data from a location and gives response in 'res'
 app.get('/api/animals', (req, res) => {
@@ -46,7 +51,19 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
 });
+//GEt method to respond with a html file 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
 //make the server listen at a port 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
